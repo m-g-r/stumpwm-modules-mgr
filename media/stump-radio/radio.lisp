babcc83a0fe9b884a7ad46ec048f368142e3f7b4
mgr <m.retzlaff@gmx.net>
2020-04-08 03:33:38 +0200

    add debugging and a lot of documentation, properly handle termination-by-signal

--- media/stump-radio/radio.lisp
+++ media/stump-radio/radio.lisp
@@ -43,19 +43,39 @@
   "holds the process structure of mplayer when a radio is running (or at least we think it is running),
   NIL if not playing anything (as it was not yet ever playing or we properly shut it down)")
 
+(defvar *radio-debug-level* 5
+  "debug level for stump-radio")
+
+(defun exited-or-signaled-sigterm-p (process)
+  "If a process handled SIGTERM correctly it should terminate normally
+  which results in process-status :exited.
+  But if it was signaled too early it could not yet handle the signal
+  and thus it is terminated by the signal itself. In that case, the
+  resulting process-status is :signaled and with the causing signal
+  as its exit-code."
+  (or (eq :exited (sb-ext:process-status process))
+      (and (eq :signaled (sb-ext:process-status process))
+           (= sb-unix:sigterm (sb-ext:process-exit-code process)))))
+
 (defun radio-running-p ()
   (and *radio*
-       ;; don't test for :running, but for (not :exited),
-       ;; as the process might also be in :stopped or :signaled
-       (not (eq (sb-ext:process-status *radio*) :exited))))
+       ;; don't test for :running, as this is not binary logic and
+       ;; the process might also be in :stopped,:signaled, or :exited
+       (not (exited-or-signaled-sigterm-p *radio*))))
 
 (defvar *sent-termination-signal* nil
   "used to detect that a status change to :exited was caused by
   our own intentional process termination")
 
 (defun radio-status-change (process)
-  (if (and (eq (sb-ext:process-status process) :exited)
-           *sent-termination-signal*)
+  (dformat *radio-debug-level* "status in~%")
+  (dformat *radio-debug-level* "pid: ~a status: ~a exit code: ~a termination: ~a~%"
+          (sb-ext:process-pid process)
+          (sb-ext:process-status process)
+          (sb-ext:process-exit-code process)
+          *sent-termination-signal*)
+  (if (and *sent-termination-signal*
+           (exited-or-signaled-sigterm-p process))
       ;; intentional process termination by us
       (progn
         (message "Radio stopped.")
@@ -63,49 +83,80 @@
       ;; unknown signal
       (message (format nil "Radio status changed to ~a.~%(Process ID: ~a)"
                        (sb-ext:process-status process)
-                       (sb-ext:process-pid process)))))
+                       (sb-ext:process-pid process))))
+  (dformat *radio-debug-level* "status out~%"))
 
-(defvar *radio-start/stop-mutex* (sb-thread:make-mutex :name "stump-radio start/stop mutex"))
+(defvar *radio-start/stop-mutex* (sb-thread:make-mutex :name "stump-radio start/stop mutex")
+  "mutex to ensure that no calls to RADIO-START and RADIO-STOP overlap")
 
 (defcommand radio-start () ()
   "start radio if not running"
   (sb-thread:with-mutex (*radio-start/stop-mutex* :wait-p nil)
+    (dformat *radio-debug-level* "start in~%")
     (assert-stations)
     (if (radio-running-p)
         (message "Warning: radio already running, not starting.")
         (destructuring-bind (name . url)
             (car *stations*)
-          (message (format nil "Starting ~a radio..." name))d
+          (message (format nil "Starting ~a radio..." name))
           (setf *sent-termination-signal* nil
                 *radio*
                 (sb-ext:run-program "mplayer" (list url)
                                     :search t
                                     :wait nil
-                                    :status-hook #'radio-status-change))))))
+                                    :status-hook #'radio-status-change))))
+    (dformat *radio-debug-level* "start out~%")))
 
 (defcommand radio-stop () ()
   "stop radio if running"
   (sb-thread:with-mutex (*radio-start/stop-mutex* :wait-p nil)
+    (dformat *radio-debug-level* "stop in~%")
     (if (not (radio-running-p))
         (message "Warning: radio not running, not stopping.")
         (progn
           (message "Stopping radio...")
           (setf *sent-termination-signal* t)
-          (sb-ext:process-kill *radio* 15) ;; SIGTERM
+          (sb-ext:process-kill *radio* sb-unix:sigterm)
 
-          ;; kludge to make RADIO-STOP wait for up to 1 s, hoping that
+          ;; kludge to make RADIO-STOP wait for up to 5 s, hoping that
           ;; the SIGTERM was handled by then and RADIO-STATUS-CHANGE ran.
           ;; Usually, this should spin for just one iteration (.05 s)
           ;; but it should prevent a late status change message
-          ;; for RADIO-NEXT-STATION and RADIO-PREVIOUS-STATION.
+          ;; for RADIO-NEXT-STATION and RADIO-PREVIOUS-STATION which
+          ;; would be irritating for the user.
+          ;;
+          ;; When I multiple times hit the key that executes RADIO-NEXT-STATION
+          ;; but with small delays in between. In many tests I could get the wait
+          ;; time up to 2.45 s on my system.
+          ;; It seems to happen when mplayer ran long enough to start trying
+          ;; to connect to the network already. If it receives SIGTERM then
+          ;; it will first wait for the network requests and only terminate
+          ;; afterwards.
+          ;; It does not happen when it receives the SIGTERM right away as then
+          ;; then the process is terminated by the signal itself which is fast
+          ;; (see EXITED-OR-SIGNALED-SIGTERM-P).
+          ;;
+          ;; We could sent SIGKILL instead which would be faster but not nice.
+          ;; If mplayer needs longer to tidily self-terminate that's how it is.
           (when (eq :failed (loop
+                               with wait = .05
+                               with max-i = 100
                                for i from 1
                                while *radio*
-                               do (sleep .05)
-                               when (>= i 20)
+                               do (progn (sleep wait)
+                                         (dformat *radio-debug-level* "RADIO-STOP waiting ~,3f s already~%" (* i wait)))
+                               when (>= i max-i)
                                return :failed))
-            (message "Warning: Waited for radio to stop but stopped waiting after 1 s.")
-            (setf *radio* nil))))))
+            ;; This still might happen, for example, when someone manually sent
+            ;; a SIGSTOP. In that case, the process will be stopped and stays that way.
+            ;; After the timeout, stump-radio forgets about it. But even then, the
+            ;; SIGTERM was sent already, so when someone sends a SIGCONT, the process
+            ;; right away handles the old SIGTERM and terminats (to status :exited).
+            ;; And when a SIGKILL it will terminate right away (to status :signaled).
+            ;; All that is okay. We don't want to mess with manually sent signals.
+            (message "Warning: Waited for radio to stop but stopped waiting after 5 s.")
+            (setf *radio* nil))))
+    (dformat *radio-debug-level* "stop out~%")))
 
 (defcommand radio-toggle-playback () ()
   "stop radio if running and start playing if not"
@@ -117,6 +168,7 @@
   "stop current radio and start playing again
   (use for network problems or after suspend)"
   (radio-stop)
+  (dformat *radio-debug-level* "between stop and start~%~%")
   (radio-start))
 
 (defcommand radio-next-station () ()
