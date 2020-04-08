00f4b0988337702c7eafafc02160a90052f0bde1
mgr <m.retzlaff@gmx.net>
2020-04-08 23:27:09 +0200

    export now command volume-switch-sound-card

--- media/stump-volume-control/package.lisp
+++ media/stump-volume-control/package.lisp
@@ -3,4 +3,5 @@
 (defpackage #:stump-volume-control
   (:use #:cl #:stumpwm)
   (:export #:volume-up #:volume-down #:volume-toggle-mute
+           #:volume-switch-sound-card
            #:*sound-card* #:*pulse-audio-unmute-outputs*))
